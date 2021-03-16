6dfddc839ddb3ce7195f93cfa72f38a1906f327b
aimenux <devplusdev@gmail.com>
2021-03-16 21:51:34 +0100

    First commit !

---
+++ AcuPackagingApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PX.Data;
+using PX.Objects.GL;
+
+namespace AcuPackagingApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            const string userName = @"admin";
+            const string tenantName = @"Company";
+
+            using (new PXLoginScope($"{userName}@{tenantName}"))
+            {
+                var graph = PXGraph.CreateInstance<PXGraph>();
+                foreach (Account row in PXSelect<Account>.Select(graph))
+                {
+                    Console.WriteLine($"{row.AccountCD} - {row.Description}");
+                }
+            }
+
+            Console.WriteLine("Press any key to exit !");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AcuPackagingDemo
+```
 Packaging acumatica dlls to nuget feed source
+```
+
+> In AcuPackagingLib, i m using github actions in order to package acumatica dlls then push them to [nuget.org](https://www.nuget.org/)
+>
+>> Nugets are generated for versions :
+>>
+>> :pushpin: [20R1-112](https://www.nuget.org/packages/Acu20R1-112/)
+>>
+>> :pushpin: [20R2-200](https://www.nuget.org/packages/Acu20R2-200/)
+>
+> In AcuPackagingApp, i m using [Acu20R2-200.nupkg](https://www.nuget.org/packages/Acu20R2-200/) in order to illustrate using BQL (Acumatica ORM) in a console application.
+
+**`Tools`** : vs19, net framework 4.8, acumatica
